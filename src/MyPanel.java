fb6dc1c34439f958b65c16bca062706867dfe890
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-02-14 17:23:21 -0600

    updated printing logic

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -11,39 +11,22 @@ public class MyPanel extends JPanel
     public void paint(Graphics g)
     {
         g.setColor(Color.BLACK);
-        g.drawRect(0,0, 800,600);
-        g.fillRect(0,0, 800,600);
-        int x = 0;
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                if ((i+j) % 2 == 0)
-                {
+        g.fillRect(0, 0, 800, 600);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0) {
                     g.setColor(Color.RED);
-                    g.fillRect(i*75, j*75, 75, 75);
+                } else {
+                    g.setColor(Color.BLACK);
                 }
-                else
-                {
-                    if (j < 3)
-                    {
-                        x++;
-                        if (x % 2 == 1)
-                        {
-                            g.setColor(Color.WHITE);
-                            g.fillArc(i*75 + 7, j*75 + 7, 60, 60, 0, 360);
-                        }
-                    }
-                    else if(j >= 5)
-                    {
-                        x++;
-                        if (x % 2 == 0)
-                        {
-                            x = 0;
-                            g.setColor(Color.RED);
-                            g.fillOval(i*75 + 7, j*75 + 7, 60, 60);
-                        }
-                    }
+                g.fillRect(i * 75, j * 75, 75, 75);
+                if (j < 3 && (i + j) % 2 == 1) {
+                    g.setColor(Color.WHITE);
+                    g.drawOval(i * 75 + 7, j * 75 + 7, 60, 60);
+                }
+                else if (j > 4 && (i + j) % 2 == 1) {
+                    g.setColor(Color.RED);
+                    g.fillOval(i * 75 + 7, j * 75 + 7, 60, 60);
                 }
             }
         }
