6015af3ca5c73c9577bfe5f67dbd4dd4f6dfc1b8
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-02-14 17:37:53 -0600

    changed logic to center the board

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 public class MyPanel extends JPanel
 {
+
     public MyPanel ()
     {
         setSize(800,600);
@@ -10,23 +11,32 @@ public class MyPanel extends JPanel
 
     public void paint(Graphics g)
     {
+        int startX = (800 - (75*8)) / 2;
+        int startY = (600 - (75*8)) / 2;
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 800, 600);
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if ((i + j) % 2 == 0) {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
                     g.setColor(Color.RED);
-                } else {
+                }
+                else
+                {
                     g.setColor(Color.BLACK);
                 }
-                g.fillRect(i * 75, j * 75, 75, 75);
-                if (j < 3 && (i + j) % 2 == 1) {
+                g.fillRect(startX + i * 75, startY + j * 75, 75, 75);
+                if (j < 3 && (i + j) % 2 == 1)
+                {
                     g.setColor(Color.WHITE);
-                    g.drawOval(i * 75 + 7, j * 75 + 7, 60, 60);
+                    g.drawOval(startX + i * 75 + 7, startY + j * 75 + 7, 60, 60);
                 }
-                else if (j > 4 && (i + j) % 2 == 1) {
+                else if (j > 4 && (i + j) % 2 == 1)
+                {
                     g.setColor(Color.RED);
-                    g.fillOval(i * 75 + 7, j * 75 + 7, 60, 60);
+                    g.fillOval(startX + i * 75 + 7, startY + j * 75 + 7, 60, 60);
                 }
             }
         }
